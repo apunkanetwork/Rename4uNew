64483bf52165a4c6f31b544f3bddee270f8abfe0
Enoxside <105333651+apunkanetwork@users.noreply.github.com>
2022-08-26 21:05:16 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@ apt update && apt upgrade -y
 apt install git -y
 pip3 install -U pip
 
-git clone https://github.com/TEAM-PYRO-BOTZ/PYRO-RENAME-BOT.git          
+git clone https://github.com/apunkanetwork/Rename4uNew.git          
 cd Github_Repo
 pip3 install -U -r requirements.txt
 
